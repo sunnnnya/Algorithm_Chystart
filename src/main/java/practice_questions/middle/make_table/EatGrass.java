c2ab224cbe438fcd9fa2f5906401d295041ec61e
Sunnnnny <2441664255@qq.com>
2024-07-24 00:47:55 +0800

    打表技巧

---
+++ src/main/java/practice_questions/middle/make_table/EatGrass.java
@@ -0,0 +1,12 @@
+package practice_questions.middle.make_table;
+
+/**
+ * @BelongsPackage: practice_questions.middle.make_table
+ * @ClassName: EatGrass
+ * @Author: 丛虹羽
+ * @Date: 2024/7/24 上午12:46
+ * @Description: 吃草
+ * 分先手和后手，一个人只能吃4 ^ n的草，现在给你一个草的数量，问谁先赢（先吃完草的人赢）
+ */
+public class EatGrass {
+}

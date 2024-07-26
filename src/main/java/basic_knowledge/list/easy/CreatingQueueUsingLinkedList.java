80e2bf3e08a2c662ae9be3d8cf80c6081dc84da3
Sunnnnny <2441664255@qq.com>
2024-07-26 22:31:10 +0800

    单链表实现队列

---
+++ src/main/java/basic_knowledge/list/easy/CreatingQueueUsingLinkedList.java
@@ -0,0 +1,123 @@
+package basic_knowledge.list.easy;
+
+/**
+ * @BelongsPackage: basic_knowledge.list.easy
+ * @ClassName: CreatingQueueUsingLinkedList
+ * @Author: 丛虹羽
+ * @Date: 2024/7/26 下午9:56
+ * @Description: 使用链表的方式创建队列
+ */
+public class CreatingQueueUsingLinkedList {
+
+    /**
+     * 链表中节点的结构
+     *
+     * @param <T>
+     */
+    public static class Node<T> {
+        T value;
+        Node<T> next;
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    /**
+     * 自定义的队列实体类
+     */
+    public static class MyQueue<V> {
+        private Node<V> head;
+        private Node<V> tail;
+        private int size;
+
+        public MyQueue() {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        /**
+         * 判断是否为空
+         *
+         * @return
+         */
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        /**
+         * 查询队列中元素的个数
+         *
+         * @return
+         */
+        public int size() {
+            return size;
+        }
+
+        /**
+         * 向队列中添加值
+         *
+         * @param value
+         */
+        public void offer(V value) {
+            Node<V> cur = new Node<V>(value);
+            if (tail == null) {
+                head = cur;
+                tail = cur;
+            } else {
+                tail.next = cur;
+                tail = tail.next;
+            }
+            size++;
+        }
+
+        /**
+         * 查看并且删除单链表中的元素
+         *
+         * @return
+         */
+        public V poll() {
+            V ans = null;
+            if (head != null) {
+                ans = head.value;
+                head = head.next;
+                size--;
+            } else { // 尾巴保持一致
+                tail = null;
+            }
+            return ans;
+        }
+
+        /**
+         * 查看队列中的队顶元素
+         *
+         * @return
+         */
+        public V peek() {
+            V ans = null;
+            if (head != null) {
+                ans = head.value;
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyQueue<Integer> queue = new MyQueue<>();
+        queue.offer(23);
+        queue.offer(22);
+        queue.offer(20);
+        queue.offer(12);
+        queue.offer(9);
+        System.out.println(queue.isEmpty()); // false
+        System.out.println(queue.size()); // 5
+        System.out.println(queue.peek()); // 23
+        System.out.println(queue.poll()); // 23
+        System.out.println(queue.poll()); // 22
+        System.out.println(queue.poll()); // 20
+        System.out.println(queue.poll()); // 12
+        System.out.println(queue.poll()); // 9
+        System.out.println(queue.size()); // 0
+        System.out.println(queue.isEmpty()); // true
+    }
+}
--- src/main/java/basic_knowledge/list/easy/ReverseDoubleList.java
+++ src/main/java/basic_knowledge/list/easy/ReverseDoubleList.java
@@ -9,9 +9,77 @@ package basic_knowledge.list.easy;
  */
 public class ReverseDoubleList {
 
+    /**
+     * 双端链表的节点信息
+     */
+    public static class DoubleNode {
+        public int value;
+        // 前引用指针
+        public DoubleNode last;
+        // 后引用指针
+        public DoubleNode next;
+        public DoubleNode(int data) {
+            this.value = data;
+        }
+    }
+
+    /**
+     * 反转双端链表
+     *
+     * @param head
+     * @return
+     */
+    public static DoubleNode reverseDoubleNode(DoubleNode head) {
+        DoubleNode pre = null;
+        DoubleNode next = null;
+        while (head != null) {
+            next = head.next;
+            // 和单链表的区别在于last和next指针的调整
+            head.next = pre;
+            head.last = next;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    /**
+     * 打印双端链表
+     *
+     * @param head
+     */
+    public static void printDoubleNode(DoubleNode head) {
+        if (head == null) {
+            return;
+        }
+        while (head != null) {
+            if (head.next == null) {
+                System.out.print(head.value);
+            } else {
+                System.out.print(head.value + " -> ");
+            }
+            head = head.next;
+        }
+    }
 
 
+    /**
+     * 测试函数
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-
+        DoubleNode doubleNode = new DoubleNode(12);
+        DoubleNode doubleNode1 = new DoubleNode(21);
+        DoubleNode doubleNode2 = new DoubleNode(11);
+        // 12 -> 21 -> 11
+        doubleNode.next = doubleNode1;
+        doubleNode.last = null;
+        doubleNode1.next = doubleNode2;
+        doubleNode1.last = doubleNode;
+        doubleNode2.next = null;
+        doubleNode2.last = doubleNode1;
+        DoubleNode newHead = reverseDoubleNode(doubleNode);
+        printDoubleNode(newHead);
     }
 }

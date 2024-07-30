7c5b0fc423d4e5c01327d6b9092b602f15ff5b86
Sunnnnny <2441664255@qq.com>
2024-07-30 21:44:44 +0800

    荷兰国旗问题

---
+++ src/main/java/basic_knowledge/sort/common/DutchFlagI.java
@@ -0,0 +1,76 @@
+package basic_knowledge.sort.common;
+
+import java.util.Arrays;
+
+/**
+ * @BelongsPackage: basic_knowledge.sort.common
+ * @ClassName: DutchFlagI
+ * @Author: 丛虹羽
+ * @Date: 2024/7/30 下午9:18
+ * @Description: 荷兰国旗问题
+ *
+ * 给定一个数组arr，和一个数num，
+ *      请把小于等于num的数放在数组的左边，
+ *      大于num的数放在数组的右边。
+ * 要求额外空间复杂度0(1)，时间复杂度O(N)
+ *
+ */
+public class DutchFlagI {
+
+    /**
+     * arr[i] < num, 把 arr[i]和 <=区域 的下一个数进行交换，然后 <= 区域 往右扩
+     * arr[i] > num i++，<= 区域不变
+     *
+     * @param arr
+     * @param num
+     * @return
+     */
+    public static void dutchFlag(int[] arr, int num) {
+        if (arr == null) {
+            return ;
+        }
+        if (arr.length == 1) {
+            return ;
+        }
+        int L = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] <= num) && (i == L)) {
+                L++;
+            } else if ((arr[i] <= num) && (i != L)){
+                swap(arr, i, L++);
+            } else {
+                continue;
+            }
+        }
+    }
+
+    /**
+     * swap two number
+     *
+     * @param arr array
+     * @param i   array index
+     * @param j   array index
+     */
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /**
+     * 测试
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 2, 5, 6, 12, 5, 3, 7, 9, 10};
+        dutchFlag(arr, 5);
+        System.out.println(Arrays.toString(arr));
+        // [5, 2, 5, 5, 3, 12, 10, 6, 7, 9, 10]
+
+        int[] arr1 = {3, 5, 5, 2};
+        dutchFlag(arr1, 4);
+        System.out.println(Arrays.toString(arr1));
+        // [3, 2, 5, 5]
+    }
+}
---
+++ src/main/java/basic_knowledge/sort/common/DutchFlagII.java
@@ -0,0 +1,17 @@
+package basic_knowledge.sort.common;
+
+/**
+ * @BelongsPackage: basic_knowledge.sort.common
+ * @ClassName: DutchFlagII
+ * @Author: 丛虹羽
+ * @Date: 2024/7/30 下午9:43
+ * @Description: 荷兰国旗问题
+ *
+ * 给定一个数组arr，和一个数num，
+ *      请把小于num的数放在数组的左边，
+ *      等于num的数放在数组的中间，
+ *      大于num的数放在数组的右边。
+ * 要求额外空间复杂度0(1)，时间复杂度0(N)
+ */
+public class DutchFlagII {
+}

80f66ac5dc267d69d85974e68bf0ae8fbde19aed
Sunnnnny <2441664255@qq.com>
2024-07-31 16:12:54 +0800

    堆排

--- src/main/java/basic_knowledge/sort/advanced_sort/HeapSort.java
+++ src/main/java/basic_knowledge/sort/advanced_sort/HeapSort.java
@@ -1,5 +1,8 @@
 package basic_knowledge.sort.advanced_sort;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * @BelongsPackage: basic_knowledge.sort.advanced_sort
  * @ClassName: Heap
@@ -28,6 +31,7 @@ public class HeapSort {
     /**
      * 现在某个数处在 index 下标索引的位置，需要向上进行替换，从而形成大根堆或者小根堆
      * 从 下 -> 上
+     * 大根堆
      * @param arr    数组
      * @param index  下标 *
      */
@@ -42,25 +46,56 @@ public class HeapSort {
     /**
      * 从 index = 0 的位置向下移动交换，形成大根堆或小根堆
      * 从 上 -> 下
+     *
      * @param arr
+     * @param index
+     * @param heapSize
      */
     public static void heapIfy(int[] arr, int index, int heapSize) {
         int leftChild = 2 * index + 1;
         // 还有左孩子的时候
         while (leftChild < heapSize) {
-            // 左右孩子节点中数值大的一个下标
-            int largeIndex = leftChild + 1 < heapSize && arr[leftChild] > arr[leftChild + 1]
+            // 有右孩子，找到左右孩子中数组更大的那个索引下标
+            int largeIndex = ((leftChild + 1 < heapSize) && (arr[leftChild] > arr[leftChild + 1]))
                     ? leftChild : leftChild + 1;
             // 找到父亲，左右孩子中数值大的索引下标
-            largeIndex = arr[index] < arr[largeIndex] ? largeIndex : index;
-            // 如果当前节点大，则不进行交换
+            largeIndex = arr[largeIndex] > arr[index] ? largeIndex : index;
+            // 如果当前节点最大，则不进行交换
             if (largeIndex == index) {
                 break;
             }
             swap(arr, index, largeIndex);
+            // 节点改为 最大的节点下标索引
             index = largeIndex;
             // 移动后得到节点的左孩子
             leftChild = 2 * index + 1;
+        }
+    }
+
+    /**
+     * 堆排序
+     *
+     * @param arr
+     */
+    public static void heapSort(int[] arr) {
+        if (arr == null || arr.length == 1) {
+            return ;
+        }
+        // 初始化堆 形成大根堆或者小根堆
+        // 本案例：初始化成大根堆
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, i);
+        }
+        System.out.println("--------check---------");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("--------check---------");
+        // heapSize表示堆的大小
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);
+        while (heapSize > 0) {
+            // 始终都是堆顶的元素做 heapIfy
+            heapIfy(arr, 0, --heapSize);
+            swap(arr, 0, heapSize);
         }
     }
 
@@ -71,5 +106,8 @@ public class HeapSort {
      */
     public static void main(String[] args) {
         int[] arr = {10, 6, 2, 3, 4, 5, 1, 8, 9, 12, 21, 67};
+        // heapSize = 12
+        heapSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }

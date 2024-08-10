aa316fb4c33b88367b2ddc3b3e9625908988f9f9
Sunnnnny <2441664255@qq.com>
2024-08-10 14:10:59 +0800

    链表：回文单链表，在链表上进行修改，空间复杂度O(1)；

--- src/main/java/practice_questions/list/easy/IsPalindrome.java
+++ src/main/java/practice_questions/list/easy/IsPalindrome.java
@@ -2,6 +2,7 @@ package practice_questions.list.easy;
 
 import practice_questions.list.common.ListNode;
 
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -20,11 +21,12 @@ public class IsPalindrome {
 
     /**
      * 查看链表是不是回文字串，牺牲空间使用stack结构存储，利用stack的特性进行处理；
+     * 空间复杂度O（N）
      *
      * @param head
      * @return
      */
-    public static boolean isPalindrome(ListNode head) {
+    public static boolean isPalindrome1(ListNode head) {
         ListNode cur = head;
         Stack<Integer> stack = new Stack<>();
         while (cur != null) {
@@ -43,6 +45,82 @@ public class IsPalindrome {
     }
 
     /**
+     * 修改单链表中的指针，判断是不是回文
+     *
+     * @param head
+     * @return
+     */
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode S = head;
+        ListNode F = head;
+        // 使用快慢指针找到中点的位置
+        while (F.next != null && F.next.next != null) {
+            S = S.next;
+            F = F.next.next;
+        }
+        // 反转链表之前的头节点信息
+        F = S.next;
+        // 慢指针的next设置为null
+        S.next = null;
+        // 反转后半段的链表
+        ListNode next = null;
+        while (F != null) {
+            next = F.next;
+            F.next = S;
+            S = F;
+            F = next;
+        }
+        // S 表示后半部分反转之后的头节点
+        next = S;
+        // F 表示前半部分未反转的头节点
+        F = head;
+        boolean ans = true;
+        // 两种情况都会退出一个是：奇数单链表时：S == null F = null 偶数单链表时：F == null S != null
+        while (S != null && F != null) {
+            if (S.val != F.val) {
+                ans = false;
+                break;
+            } else {
+                S = S.next;
+                F = F.next;
+            }
+        }
+        S = next.next;
+        next.next = null;
+        while (S != null) {
+            F = S.next;
+            S.next = next;
+            next = S;
+            S = F;
+        }
+        return ans;
+    }
+
+    /**
+     * 反转单链表
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseListNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode next = null;
+        ListNode pre = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    /**
      * 测试
      *
      * @param args
@@ -57,6 +135,7 @@ public class IsPalindrome {
         listNode3.next = listNode4;
         System.out.println(isPalindrome(listNode1));
         // true
+
 
         ListNode listNode5 = new ListNode(1);
         ListNode listNode6 = new ListNode(5);

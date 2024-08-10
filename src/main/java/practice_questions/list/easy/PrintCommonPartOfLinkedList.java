981c5b3bf1704ce6136497fa1159132d4fcdaa40
Sunnnnny <2441664255@qq.com>
2024-08-10 09:17:02 +0800

    链表：获取链表公共部分信息；

---
+++ src/main/java/practice_questions/list/easy/PrintCommonPartOfLinkedList.java
@@ -0,0 +1,99 @@
+package practice_questions.list.easy;
+
+import practice_questions.list.common.ListNode;
+
+/**
+ * @BelongsPackage: practice_questions.list.easy
+ * @ClassName: PrintCommonPartOfLinkedList
+ * @Author: 丛虹羽
+ * @Date: 2024/8/10 上午8:59
+ * @Description: 打印两个顺序链表的公共部分
+ *
+ *
+ */
+public class PrintCommonPartOfLinkedList {
+
+    /**
+     * 返回两个链表公共部分的头节点
+     *
+     * @param head1
+     * @param head2
+     * @return
+     */
+    public static ListNode printCommonPartOfLinkedList(ListNode head1, ListNode head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        ListNode head = null;
+        ListNode cur = null;
+        while (head1 != null && head2 != null) {
+            if(head1.val < head2.val) {
+                head1 = head1.next;
+            } else if (head1.val > head2.val) {
+                head2 = head2.next;
+            } else {
+                if (head == null) {
+                    head = new ListNode(head1.val);
+                    cur = head;
+                } else {
+                    cur.next = new ListNode(head1.val);
+                    cur = cur.next;
+                }
+                head1 = head1.next;
+                head2 = head2.next;
+            }
+        }
+        return head;
+    }
+
+    /**
+     * 打印链表信息
+     *
+     * @param head
+     */
+    public static void printList(ListNode head) {
+        while (head != null) {
+            if (head.next == null) {
+                System.out.print(head.val);
+            } else {
+                System.out.print(head.val + " -> ");
+            }
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    /**
+     * 测试
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(5);
+        ListNode listNode3 = new ListNode(7);
+        ListNode listNode4 = new ListNode(8);
+        ListNode listNode5 = new ListNode(10);
+        ListNode listNode6 = new ListNode(12);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
+        System.out.print("链表1：");
+        printList(listNode1);
+
+        ListNode listNode7 = new ListNode(3);
+        ListNode listNode8 = new ListNode(5);
+        ListNode listNode9 = new ListNode(10);
+        ListNode listNode10 = new ListNode(12);
+        listNode7.next = listNode8;
+        listNode8.next = listNode9;
+        listNode9.next = listNode10;
+        System.out.print("链表2：");
+        printList(listNode7);
+
+        System.out.print("获取链表公共信息：");
+        printList(printCommonPartOfLinkedList(listNode1, listNode7));
+    }
+}

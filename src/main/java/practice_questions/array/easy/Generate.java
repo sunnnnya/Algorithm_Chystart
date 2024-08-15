c58734132252cbf1841cef056be54b86d255ddf9
Sunnnnny <2441664255@qq.com>
2024-08-15 09:21:47 +0800

    容器：杨辉三角的基本使用,及其拓展；

--- src/main/java/practice_questions/array/easy/Generate.java
+++ src/main/java/practice_questions/array/easy/Generate.java
@@ -39,12 +39,42 @@ public class Generate {
     }
 
     /**
+     * 返回杨辉三角的第i行的数据
+     *
+     * @param rowIndex
+     * @return
+     */
+    public static List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> row = new ArrayList<>(34);
+        row.add(Arrays.asList(1));
+        for (int i = 1; i < 34; i++) {
+            ArrayList<Integer> column = new ArrayList<>(i + 1);
+            column.add(1);
+            for (int j = 1; j < i; j++) {
+                column.add(row.get(i - 1).get(j - 1) + row.get(i - 1).get(j));
+            }
+            column.add(1);
+            row.add(column);
+        }
+        return row.get(rowIndex);
+    }
+
+    /**
      * 测试
      *
      * @param args
      */
     public static void main(String[] args) {
+        System.out.println("==============打印杨辉三角============");
         System.out.println(generate(5));
         // [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+
+        System.out.println("==============返回杨辉三角指定行的信息============");
+        System.out.println(getRow(3));
+        // [1, 3, 3, 1]
+        System.out.println(getRow(0));
+        // [1]
+        System.out.println(getRow(1));
+        // [1, 1]
     }
 }

1b1c4e466e51b1d3d944af04dcaab7735ec98b37
Sunnnnny <2441664255@qq.com>
2024-08-27 23:19:36 +0800

    【数学】：最小公倍数；

---
+++ src/main/java/basic_knowledge/math/LCM.java
@@ -0,0 +1,44 @@
+package basic_knowledge.math;
+
+/**
+ * @BelongsPackage: basic_knowledge.math
+ * @ClassName: LCM
+ * @Author: 丛虹羽
+ * @Date: 2024/8/27 下午11:14
+ * @Description: 最小公倍数
+ */
+public class LCM {
+
+    /**
+     * 最大公因数
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    /**
+     * 最小公倍数
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static long lcm(long a, long b) {
+        return a / gcd(a, b) * b;
+    }
+
+    /**
+     * 测试
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        long lcm = lcm(10, 7);
+        System.out.println("lcm = " + lcm);
+        // lcm = 70
+    }
+}

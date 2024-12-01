fab1f4684462d689269ab7660949817832e96ba9
Sunnnnny <2441664255@qq.com>
2024-12-01 13:29:57 +0800

    【插入排序】：（1）注意边界条件的细心处理

--- src/main/java/basic_knowledge/sort/base_sort/InsertSort.java
+++ src/main/java/basic_knowledge/sort/base_sort/InsertSort.java
@@ -7,22 +7,9 @@ import java.util.Arrays;
  * @ClassName: SelectSort
  * @Author: 丛虹羽
  * @Date: 2024/7/21 下午10:58
- * @Description: 选择排序
+ * @Description: 插入排序
  */
 public class InsertSort {
-
-    /**
-     * 交换数组中对应两个索引的数
-     *
-     * @param arr 数组
-     * @param i 交换的索引下标
-     * @param j 交换的索引下标
-     */
-    private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
 
     /**
      * 插入排序
@@ -30,16 +17,29 @@ public class InsertSort {
      * @param arr 待排序的数组
      */
     public static void insertSort(int[] arr) {
-        if (arr == null || arr.length == 1) {
+        if (arr == null || arr.length < 2) {
             return;
         }
         // 从第二张牌开始，到最后一张牌
         for (int i = 1; i < arr.length; i++) {
-            // 往前进行交换插入，第二牌进行比较停止， 从大到小进行排序
+            // 往前进行交换插入，第二牌进行比较停止，从大到小进行排序
             for (int j = i; j >= 1 && arr[j - 1] < arr[j]; j--) {
                 swap(arr, j - 1, j);
             }
         }
+    }
+
+    /**
+     * 交换数组中对应两个索引的数
+     *
+     * @param arr 数组
+     * @param i   交换的索引下标
+     * @param j   交换的索引下标
+     */
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     /**

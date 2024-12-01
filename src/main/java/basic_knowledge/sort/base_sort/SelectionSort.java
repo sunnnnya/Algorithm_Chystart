5c078f898526f81550be8170b739fea431558aa9
Sunnnnny <2441664255@qq.com>
2024-12-01 12:42:21 +0800

    【选择排序】：（1）注意边界点的处理（2）0 ~ N - 1

--- src/main/java/basic_knowledge/sort/base_sort/SelectionSort.java
+++ src/main/java/basic_knowledge/sort/base_sort/SelectionSort.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  * @ClassName: SelectionSort
  * @Author: 丛虹羽
  * @Date: 2024/7/24 下午10:41
- * @Description: 选择排序
+ * @Description: 选择排序 - 好好处理边界
  */
 public class SelectionSort {
 
@@ -18,7 +18,7 @@ public class SelectionSort {
      * @return
      */
    public static int[] selectionSort(int[] arr) {
-       if (arr == null || arr.length == 1 || arr.length == 0) {
+       if (arr == null || arr.length < 2) {
            return arr;
        }
        for (int i = 0; i < arr.length - 1; i++) {
@@ -35,9 +35,9 @@ public class SelectionSort {
      * 交换数组中的两个索引位置的元素，
      * 可以考虑使用 异或 运算进行交换，但是可能会出现被消成 0 的情况
      *
-     * @param arr
-     * @param i
-     * @param j
+     * @param arr 待交换的数据
+     * @param i   i 位置的 index
+     * @param j   j 位置的 index
      */
    public static void swap(int[] arr, int i, int j) {
        int temp = arr[i];
@@ -52,11 +52,25 @@ public class SelectionSort {
      */
     public static void main(String[] args) {
         int[] arr = {10, 9, 8, 11, 12, 21, 7};
-        System.out.println(Arrays.toString(selectionSort(arr)));
-        // [7, 8, 9, 10, 11, 12, 21]
+        System.out.println("排序前：" + Arrays.toString(arr));
+        System.out.println("排序后：" + Arrays.toString(selectionSort(arr)));
+        // 排序前：[10, 9, 8, 11, 12, 21, 7]
+        // 排序后：[7, 8, 9, 10, 11, 12, 21]
+
+        System.out.println();
 
         int[] arr1 = {-4, 9, 8, 11, -2, 21, 0};
-        System.out.println(Arrays.toString(selectionSort(arr1)));
-        // [-4, -2, 0, 8, 9, 11, 21]
+        System.out.println("排序前：" + Arrays.toString(arr1));
+        System.out.println("排序后：" + Arrays.toString(selectionSort(arr1)));
+        // 排序前：[-4, 9, 8, 11, -2, 21, 0]
+        // 排序后：[-4, -2, 0, 8, 9, 11, 21]
+
+        System.out.println();
+
+        int[] arr2 = new int[]{10, 2, 1, 0, -3};
+        System.out.println("排序前：" + Arrays.toString(arr2));
+        System.out.println("排序后：" + Arrays.toString(selectionSort(arr2)));
+        // 排序前：[10, 2, 1, 0, -3]
+        // 排序后：[-3, 0, 1, 2, 10]
     }
 }

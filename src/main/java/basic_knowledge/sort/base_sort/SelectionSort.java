e42889f8da87a83359cdc570d0fbd00887fb5691
Sunnnnny <2441664255@qq.com>
2024-12-01 11:33:52 +0800

    【选择排序】：（1）注意边界点的处理

--- src/main/java/basic_knowledge/sort/base_sort/SelectionSort.java
+++ src/main/java/basic_knowledge/sort/base_sort/SelectionSort.java
@@ -12,38 +12,37 @@ import java.util.Arrays;
 public class SelectionSort {
 
     /**
-     * 选择排序
+     * 选择排序 - O(n^2)
      *
      * @param arr
      * @return
      */
-    public static int[] selectionSort(int[] arr) {
-        if (arr == null || arr.length == 1) {
-            return arr;
-        }
-        for (int i = 0; i < arr.length - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                // 从小到大
-                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
-            }
-            swap(arr, i, minIndex);
-        }
-        return arr;
-    }
+   public static int[] selectionSort(int[] arr) {
+       if (arr == null || arr.length == 1 || arr.length == 0) {
+           return arr;
+       }
+       for (int i = 0; i < arr.length - 1; i++) {
+           int minIndex = i;
+           for (int j = i + 1; j < arr.length; j++) {
+               minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+           }
+           swap(arr, minIndex, i);
+       }
+       return arr;
+   }
 
     /**
-     * 交换数组中两个索引位置的元素
+     * 交换数组中的两个索引位置的元素，
+     * 可以考虑使用 异或 运算进行交换，但是可能会出现被消成 0 的情况
      *
      * @param arr
      * @param i
      * @param j
-     * @return
      */
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+   public static void swap(int[] arr, int i, int j) {
+       int temp = arr[i];
+       arr[i] = arr[j];
+       arr[j] = temp;
     }
 
     /**

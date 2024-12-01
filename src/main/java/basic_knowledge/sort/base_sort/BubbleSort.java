b249340386b6b1f1d5c0e249d253fc163a4c9f01
Sunnnnny <2441664255@qq.com>
2024-12-01 13:25:30 +0800

    【冒泡排序】：（1）注意边界条件的细心处理

--- src/main/java/basic_knowledge/sort/base_sort/BubbleSort.java
+++ src/main/java/basic_knowledge/sort/base_sort/BubbleSort.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * @BelongsPackage: basic_knowledge.sort.base_sort
  * @ClassName: BubbleSort
  * @Author: 丛虹羽
- * @Date: 2024/7/24 下午11:10
+ * @Date: 2024/12/1 下午11:10
  * @Description: 冒泡排序
  */
 public class BubbleSort {
@@ -14,21 +14,17 @@ public class BubbleSort {
     /**
      * 冒泡排序
      *
-     * @param arr
+     * @param arr 待排序的数组
      * @return
      */
     public static int[] bubbleSort(int[] arr) {
-        if (arr == null || arr.length == 1) {
+        if (arr == null || arr.length < 2) {
             return arr;
         }
-        // 指定走的趟数
+        // 从大到小
         for (int i = arr.length - 1; i >= 1; i--) {
-            // 第一次从 0 ~ n - 2
-            // 第二次从 0 ~ n - 3
-            //   最后从 0 ~ 1
             for (int j = 0; j < i; j++) {
-                // 从大到小
-                if (arr[j] < arr[j + 1]) {
+                if(arr[j] < arr[j + 1]) {
                     swap(arr, j, j + 1);
                 }
             }
@@ -39,10 +35,9 @@ public class BubbleSort {
     /**
      * 交换数组中两个索引位置的元素
      *
-     * @param arr
-     * @param i
-     * @param j
-     * @return
+     * @param arr 待交换的数组
+     * @param i   i 位置的 index
+     * @param j   j 位置的 index
      */
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
@@ -57,8 +52,9 @@ public class BubbleSort {
      */
     public static void main(String[] args) {
         int[] arr = {19, 2, 4, 2, 1, -1, 88, 12, 29};
-        bubbleSort(arr);
-        System.out.println(Arrays.toString(arr));
-        // [88, 29, 19, 12, 4, 2, 2, 1, -1]
+        System.out.println("排序前：" + Arrays.toString(arr));
+        System.out.println("排序后：" + Arrays.toString(bubbleSort(arr)));
+        // 排序前：[19, 2, 4, 2, 1, -1, 88, 12, 29]
+        // 排序后：[88, 29, 19, 12, 4, 2, 2, 1, -1]
     }
 }

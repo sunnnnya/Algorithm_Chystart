c9d913aad7870f07203625562f26efa56d90dd0e
Sunnnnny <2441664255@qq.com>
2024-12-02 20:41:09 +0800

    【哈希表】：（1）哈希表的简单使用（2）使用哈希表要分析好用途，比如存储什么信息比较合理一些。

--- src/main/java/practice_questions/hash_table/easy/TwoSum.java
+++ src/main/java/practice_questions/hash_table/easy/TwoSum.java
@@ -7,23 +7,22 @@ import java.util.HashMap;
  * @BelongsPackage: practice_questions.hash_table.easy
  * @ClassName: TwoSum
  * @Author: 丛虹羽
- * @Date: 2024/8/15 上午11:07
+ * @Date: 2024/12/2 上午11:07
  * @Description: 两数之和
  *
- * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出和为目标值 target 的那两个整数，并返回它们的数组下标。
  * 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
- * 你可以按任意顺序返回答案。
  *
  * leetcode:https://leetcode.cn/problems/two-sum
  */
 public class TwoSum {
 
     /**
-     * 使用hash表返回两数之和，Hash的value记录的是索引值
+     * 使用 hash 表返回两数之和，Hash 的 value 记录的是索引值
      *
-     * @param nums
-     * @param target
-     * @return
+     * @param nums   数组
+     * @param target 目标值
+     * @return       可以组成目标函数的索引数组
      */
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -43,8 +42,11 @@ public class TwoSum {
      */
     public static void main(String[] args) {
         int[] arr = new int[]{2,7,11,15};
-        int target = 9;
-        System.out.println(Arrays.toString(twoSum(arr, target)));
+        System.out.println(Arrays.toString(twoSum(arr, 9)));
         // [0, 1]
+
+        int[] arr1 = new int[]{2, 4, 5, 8};
+        System.out.println(Arrays.toString(twoSum(arr1, 11)));
+        // [-1, -1]
     }
 }

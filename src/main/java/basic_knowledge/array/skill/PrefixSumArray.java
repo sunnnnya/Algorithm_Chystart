054f991b92094b45c108d2dbbc7f1ebdc27d6182
Sunnnnny <2441664255@qq.com>
2024-12-03 11:04:33 +0800

    【前缀和数组】：（1）通过预处理的方式对原始数组进行处理（2）用来解决数组指定索引之间的和的问题。

--- src/main/java/basic_knowledge/array/skill/PrefixSumArray.java
+++ src/main/java/basic_knowledge/array/skill/PrefixSumArray.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * @BelongsPackage: basic_knowledge.array.skill
  * @ClassName: PrefixSumArray
  * @Author: 丛虹羽
- * @Date: 2024/8/26 上午10:08
+ * @Date: 2024/12/3 上午10:08
  * @Description: 前缀和数组的使用
  */
 public class PrefixSumArray {
@@ -18,8 +18,8 @@ public class PrefixSumArray {
      * @return     返回原数组的前缀和数组
      */
     public static int[] getPrefixArray(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            nums[i] = nums[i] + nums[i - 1];
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
         }
         return nums;
     }
@@ -27,28 +27,25 @@ public class PrefixSumArray {
     /**
      * 拷贝数组的方式返回前缀和数组
      *
-     * @param nums
-     * @return
+     * @param nums 原始数组
+     * @return     前缀和数组
      */
     public static int[] getPrefixArrayClone(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return null;
+        int[] cloneArr = new int[nums.length];
+        cloneArr[0] = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            cloneArr[i] = nums[i] + cloneArr[i - 1];
         }
-        int[] arr = new int[nums.length];
-        arr[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            arr[i] = arr[i - 1] + nums[i];
-        }
-        return arr;
+        return cloneArr;
     }
 
     /**
      * 求指定范围的和，使用下标
      *
-     * @param arr
-     * @param l
-     * @param r
-     * @return
+     * @param arr 前缀和数组
+     * @param l   左侧索引下标
+     * @param r   右侧索引下标
+     * @return    l - r 的和
      */
     public static int getSumUseIndex(int[] arr, int l, int r) {
         return l == 0 ? arr[r] : arr[r] - arr[l - 1];
@@ -57,10 +54,10 @@ public class PrefixSumArray {
     /**
      * 使用数组位置，返回前缀和信息
      *
-     * @param arr
-     * @param l
-     * @param r
-     * @return
+     * @param arr 前缀和数组
+     * @param l   左侧索引位置
+     * @param r   右侧索引位置
+     * @return    l - r 的结果
      */
     public static int getSumUseNumber(int[] arr, int l, int r) {
         return l == 1 ? arr[r - 1] : arr[r - 1] - arr[l - 2];
@@ -72,23 +69,29 @@ public class PrefixSumArray {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("================求前缀和数组=================");
+        System.out.println("=====================求前缀和数组======================");
         int[] arr = new int[]{7, 2, 1, 4, 2, 6, 8, 10};
-        System.out.println(Arrays.toString(getPrefixArray(arr)));
-        // [7, 9, 10, 14, 16, 22, 30, 40]
+        System.out.println("原数组：" + Arrays.toString(arr));
+        System.out.println("前缀和数组：" + Arrays.toString(getPrefixArray(arr)));
+        // 原数组：[7, 2, 1, 4, 2, 6, 8, 10]
+        // 前缀和数组：[7, 9, 10, 14, 16, 22, 30, 40]
+
+        System.out.println();
 
         int[] arr1 = new int[]{1, 2, 5, 21, 7, 9, 10};
-        System.out.println(Arrays.toString(getPrefixArrayClone(arr1)));
-        // [1, 3, 8, 29, 36, 45, 55]
+        System.out.println("原数组：" + Arrays.toString(arr1));
+        System.out.println("前缀和数组：" + Arrays.toString(getPrefixArrayClone(arr1)));
+        // 原数组：[1, 2, 5, 21, 7, 9, 10]
+        // 前缀和数组：[1, 3, 8, 29, 36, 45, 55]
 
-        System.out.println("================求指定范围的和=================");
-        System.out.println(getSumUseIndex(getPrefixArrayClone(arr1), 1, 4));
-        // 35
+        System.out.println("====================求指定范围的和=====================");
+        System.out.println("index(1, 4)：" + getSumUseIndex(getPrefixArrayClone(arr1), 1, 4));
+        // index(1, 4)：35
 
-        System.out.println(getSumUseIndex(getPrefixArrayClone(arr1), 0, 4));
-        // 36
+        System.out.println("index(0, 4)：" + getSumUseIndex(getPrefixArrayClone(arr1), 0, 4));
+        // index(0, 4)：36
 
-        System.out.println(getSumUseNumber(getPrefixArrayClone(arr1), 1, 5));
-        // 36
+        System.out.println("number(1, 5)：" + getSumUseNumber(getPrefixArrayClone(arr1), 1, 5));
+        // number(1, 5)：36
     }
 }

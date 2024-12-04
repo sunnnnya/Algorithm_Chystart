d177c9d2f754f335edbf2e926e5f0856ada4141d
Sunnnnny <2441664255@qq.com>
2024-12-04 11:42:16 +0800

    【归并排序 - 逆序对问题】：（1）对传统递归排序进行改写，从大到小进行梳理，并且收集信息（2）要特别注意当左右两侧相等的时候是拷贝左边还是右边的，然后通过下标进行计算，累加。

--- src/main/java/practice_questions/merge_sort/hard/ReversePairs.java
+++ src/main/java/practice_questions/merge_sort/hard/ReversePairs.java
@@ -6,10 +6,11 @@ import java.util.Arrays;
  * @BelongsPackage: practice_questions.merge_sort.hard
  * @ClassName: ReversePairs
  * @Author: 丛虹羽
- * @Date: 2024/8/14 上午9:10
- * @Description: 交易逆序对的总数 **
+ * @Date: 2024/12/4 上午9:10
+ * @Description: 交易逆序对的总数
  *
- * 在股票交易中，如果前一天的股价高于后一天的股价，则可以认为存在一个「交易逆序对」。请设计一个程序，输入一段时间内的股票交易记录 record，返回其中存在的「交易逆序对」总数。
+ * 在股票交易中，如果前一天的股价高于后一天的股价，则可以认为存在一个「交易逆序对」。
+ * 请设计一个程序，输入一段时间内的股票交易记录 record，返回其中存在的「交易逆序对」总数。
  *
  * leetcode:https://leetcode.cn/problems/shu-zu-zhong-de-ni-xu-dui-lcof
  */
@@ -22,17 +23,17 @@ public class ReversePairs {
      * @return
      */
     public static int reversePairs(int[] record) {
-        if (record == null || record.length == 1 || record.length == 0) {
+        if (record == null || record.length < 2) {
             return 0;
         }
         return mergeSort(record, 0, record.length - 1);
     }
 
     /**
-     * 归并排序的改写
+     * 归并排序的改写，需要抽象出来递归函数的返回值的结果到底要的是什么
      *
-     * @param arr
-     * @param l
+     * @param arr 原始数组
+     * @param l   左边界
      * @param r
      * @return
      */
@@ -55,10 +56,7 @@ public class ReversePairs {
      */
     public static int merge(int[] arr, int l, int mid, int r) {
         int[] nums = new int[r - l + 1];
-        int p1 = l;
-        int p2 = mid + 1;
-        int index = 0;
-        int sum = 0;
+        int p1 = l, p2 = mid + 1, index = 0, sum = 0;
         while (p1 <= mid && p2 <= r) {
             sum += arr[p1] > arr[p2] ? (r - p2 + 1) : 0;
             nums[index++] = arr[p1] > arr[p2] ? arr[p1++] : arr[p2++];
@@ -82,13 +80,11 @@ public class ReversePairs {
      */
     public static void main(String[] args) {
         int[] record = new int[]{9, 7, 5, 4, 6};
-        System.out.println(reversePairs(record));
-        // 8
-        System.out.println(Arrays.toString(record));
-        // [9, 7, 6, 5, 4]
+        System.out.println("数组：" + Arrays.toString(record) + " 的逆序对个数是：" + reversePairs(record));
+        // 数组：[9, 7, 5, 4, 6] 的逆序对个数是：8
 
-        int[] arr = new int[]{};
-        System.out.println(reversePairs(arr));
-        // 0
+        int[] arr1 = new int[]{};
+        System.out.println("数组：" + Arrays.toString(arr1) + " 的逆序对个数是：" + reversePairs(arr1));
+        // 数组：[] 的逆序对个数是：0
     }
 }

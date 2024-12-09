9162761fc5d6097a9294b4f6a597910e090f29b1
Sunnnnny <2441664255@qq.com>
2024-12-09 12:06:39 +0800

    【不定长滑动窗口】：（1）以后关于容器推荐使用 HashSet 和 HashMap（2）同样要注意函数的返回值是 max、而不是 sum，因为可能最大子数组出现在数组的中部，而不是最后的值。

---
+++ src/main/java/practice_questions/slide_window/medium/MaximumUniqueSubarray.java
@@ -0,0 +1,54 @@
+package practice_questions.slide_window.medium;
+
+import java.util.HashSet;
+
+/**
+ * @BelongsPackage: practice_questions.slide_window.medium
+ * @ClassName: MaximumUniqueSubarray
+ * @Author: 丛虹羽
+ * @Date: 2024/12/9 上午11:41
+ * @Description: 删除子数组的最大得分
+ *
+ * 给你一个正整数数组 nums ，请你从中删除一个含有 若干不同元素 的子数组。删除子数组的 得分 就是子数组各元素之 和 。
+ *
+ * leetcode： https://leetcode.cn/problems/maximum-erasure-value
+ */
+public class MaximumUniqueSubarray {
+
+    /**
+     * 不定长滑动窗口
+     *
+     * @param nums
+     * @return
+     */
+    public static int maximumUniqueSubarray(int[] nums) {
+        // 这里使用 CopyOnWriteArraySet 会 timeout
+        HashSet<Integer> set = new HashSet<>();
+        int sum = 0, max = 0;
+        for (int i = 0, left = 0; i < nums.length; i++) {
+            while(set.contains(nums[i])) {
+                sum -= nums[left];
+                set.remove(nums[left++]);
+            }
+            set.add(nums[i]);
+            sum += nums[i];
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+
+    /**
+     * 测试
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] arr = new int[]{4,2,4,5,6};
+        System.out.println(maximumUniqueSubarray(arr));
+        // 17
+
+        int[] arr1 = new int[]{5,2,1,2,5,2,1,2,5};
+        System.out.println(maximumUniqueSubarray(arr1));
+        // 8
+    }
+}

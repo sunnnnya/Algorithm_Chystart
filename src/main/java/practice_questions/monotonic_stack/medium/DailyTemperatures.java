a3268550f5f8f271f7d5c5a3d64b60576e337649
Sunnnnny <2441664255@qq.com>
2024-12-20 18:00:08 +0800

    【单调栈】：（1）注意相等值的处理过程（2）分析得到，只是求得比自己大的坐标插值，所以相等的时候依然进入，没问题。

--- src/main/java/practice_questions/monotonic_stack/medium/DailyTemperatures.java
+++ src/main/java/practice_questions/monotonic_stack/medium/DailyTemperatures.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
  * @BelongsPackage: practice_questions.monotonic_stack
  * @ClassName: DailyTemperatures
  * @Author: 丛虹羽
- * @Date: 2024/8/25 上午11:22
- * @Description: 每日温度 时间复杂度 O（N）
+ * @Date: 2024/12/20 上午11:22
+ * @Description: 每日温度 —— 单调栈 —— 时间复杂度 O（N）
  *
  * 给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
  *

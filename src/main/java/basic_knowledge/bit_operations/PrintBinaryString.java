4dfc1a3e31c6183d9812b77ae24e86f7a6cfcecf
Sunnnnny <2441664255@qq.com>
2024-12-21 22:57:54 +0800

    【位运算】：（1）返回一个十进制位对应的二进制信息。

--- src/main/java/basic_knowledge/bit_operations/PrintBinaryString.java
+++ src/main/java/basic_knowledge/bit_operations/PrintBinaryString.java
@@ -10,22 +10,8 @@ package basic_knowledge.bit_operations;
 public class PrintBinaryString {
 
     /**
-     * 打印字符串信息
-     * 重点信息：n & 1来获取最后一位的进制位信息
-     *
-     * @param number
-     * @return
-     */
-    public static String printBinaryString(int number) {
-        StringBuffer stringBuffer = new StringBuffer();
-        for (int i = 0; number > 0; number >>= 1, i++) {
-            stringBuffer.append((number & 1) == 0 ? "0" : "1");
-        }
-        return stringBuffer.reverse().toString();
-    }
-
-    /**
      * 打印十进制的数对应的二进制字符串
+     * int 类型 4 个字节，32 bit， 0 ~ 31，右移 31 位就到了最高位了
      *
      * @param number
      */
@@ -33,6 +19,7 @@ public class PrintBinaryString {
         for (int i = 31; i >= 0; i--) {
             System.out.print(((number & (1 << i))) > 0 ? "1" : "0");
         }
+        System.out.println();
     }
 
     /**
@@ -41,11 +28,24 @@ public class PrintBinaryString {
      * @param args
      */
     public static void main(String[] args) {
-        int number = 21;
-        System.out.println(printBinaryString(number));
-        // 10101
+        // Test Case 1
+        System.out.print("Test Case 1 (Input: 0): ");
+        printBinStr(0); // Expected: 00000000000000000000000000000000
 
-        printBinStr(920);
-        // 00000000000000000000001110011000
+        // Test Case 2
+        System.out.print("Test Case 2 (Input: 2147483647): ");
+        printBinStr(2147483647); // Expected: 01111111111111111111111111111111
+
+        // Test Case 3
+        System.out.print("Test Case 3 (Input: -1): ");
+        printBinStr(-1); // Expected: 11111111111111111111111111111111
+
+        // Test Case 4
+        System.out.print("Test Case 4 (Input: -920): ");
+        printBinStr(-920); // Expected: 11111111111111111111110010011000
+
+        // Test Case 5
+        System.out.print("Test Case 5 (Input: -2147483648): ");
+        printBinStr(-2147483648); // Expected: 10000000000000000000000000000000
     }
 }

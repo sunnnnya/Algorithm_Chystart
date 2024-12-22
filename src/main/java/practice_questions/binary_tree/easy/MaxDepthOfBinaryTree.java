695d8700d4537ace9d9c3e5b8b5e4fbc0ad8ab8b
Sunnnnny <2441664255@qq.com>
2024-12-22 20:17:21 +0800

    【二叉树的最大深度】：（1）使用递归求解遍历树（2）理解递归序列

--- src/main/java/practice_questions/binary_tree/easy/MaxDepthOfBinaryTree.java
+++ src/main/java/practice_questions/binary_tree/easy/MaxDepthOfBinaryTree.java
@@ -7,9 +7,8 @@ import practice_questions.binary_tree.base.TreeNode;
  * @ClassName: MaximumDepthOfBinaryTree
  * @Author: 丛虹羽
  * @Date: 2024/7/28 上午1:07
- * @Description:
- * 给定一个二叉树 root ，返回其最大深度。
- * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+ * @Description: 二叉树的最大深度
+ *
  * leetcode：https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/
  */
 public class MaxDepthOfBinaryTree {
@@ -21,10 +20,7 @@ public class MaxDepthOfBinaryTree {
      * @return
      */
     private static int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
     /**
@@ -36,12 +32,13 @@ public class MaxDepthOfBinaryTree {
         TreeNode treeNode1 = new TreeNode(3);
         TreeNode treeNode2 = new TreeNode(9);
         TreeNode treeNode3 = new TreeNode(20);
-        treeNode1.left = treeNode2;
         TreeNode treeNode4 = new TreeNode(15);
         TreeNode treeNode5 = new TreeNode(7);
+        treeNode1.left = treeNode2;
         treeNode3.left = treeNode4;
         treeNode3.right = treeNode5;
         treeNode1.right = treeNode3;
         System.out.println(maxDepth(treeNode1));
+        // 3
     }
 }

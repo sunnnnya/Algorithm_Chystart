c87bb11941b75fd2b0c721139115bb828230661e
Sunnnnny <2441664255@qq.com>
2024-12-23 18:32:33 +0800

    【计算完全二叉树节点个数】：（1）为了避免使用纯递归的方式进行计算，使用高度信息进行区分是左树还是右树是满二叉树，然后另一侧的树使用递归进行遍历，这样时间复杂度 < O(n)

--- src/main/java/practice_questions/binary_tree/easy/CountNodes.java
+++ src/main/java/practice_questions/binary_tree/easy/CountNodes.java
@@ -10,25 +10,60 @@ import practice_questions.binary_tree.base.TreeNode;
  * @Description: 完全二叉树的节点个数
  *
  * leetcode： https://leetcode.cn/problems/count-complete-tree-nodes
- *
- * 给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
- *
- * 完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，
- * 并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
  */
 public class CountNodes {
 
     /**
-     * 求二叉树中的节点个数
+     * 求二叉树中的节点个数，使用传统的方式进行遍历，时间复杂度 O（n）
+     *
+     * @param root
+     * @return
+     */
+    public static int countNodes1(TreeNode root) {
+        return root == null ? 0 : countNodes1(root.right) + countNodes1(root.left) + 1;
+    }
+
+    /**
+     * 使用性质进行处理，因为题目中已经明确是完全二叉树，索引拿到左侧最左的高度，然后右子树的高度
+     *  （1）如果高度信息一样，则左子树使用 满二叉树性质，右侧使用递归
+     *  （2）如果高度不一样，右子树使用满二叉树性质，左侧使用递归
      *
      * @param root
      * @return
      */
     public static int countNodes(TreeNode root) {
-        if (root == null) {
+        if(root == null) {
             return 0;
         }
-        return countNodes(root.right) + countNodes(root.left) + 1;
+        int leftDepth = getDepth(root.left);
+        int rightDepth = getDepth(root.right);
+        return leftDepth == rightDepth ?
+                (int)Math.pow(2, leftDepth) - 1 + getCount(root.right) + 1:
+                (int)Math.pow(2, rightDepth) - 1 + getCount(root.left) + 1;
+    }
+
+    /**
+     * 获取左树或者右树的高度信息
+     *
+     * @param node
+     * @return
+     */
+    private static int getDepth(TreeNode node) {
+        int count = 0;
+        for(; node != null; node = node.left) {
+            count++;
+        }
+        return count;
+    }
+
+    /**
+     * 获取子树的节点的个数
+     *
+     * @param node
+     * @return
+     */
+    private static int getCount(TreeNode node) {
+        return node == null ? 0 : getCount(node.left) + getCount(node.right) + 1;
     }
 
     /**

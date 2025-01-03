52603b439a052e9f48b8fe5faf02c27b4c788bc4
Sunnnnny <2441664255@qq.com>
2025-01-03 15:49:17 +0800

    【动态规划】：（1）难点：尝试策略递归函数的编写（2）暴力递归 -> 记忆化搜索 -> 动态规划

--- src/main/java/practice_questions/dynamic_program/medium/MinCostTickets.java
+++ src/main/java/practice_questions/dynamic_program/medium/MinCostTickets.java
@@ -9,14 +9,6 @@ import java.util.Arrays;
  * @Date: 2024/8/19 下午11:54
  * @Description: 最低票价
  *
- * 在一个火车旅行很受欢迎的国度，你提前一年计划了一些火车旅行。在接下来的一年里，你要旅行的日子将以一个名为 days 的数组给出。每一项是一个从 1 到 365 的整数。
- *      火车票有 三种不同的销售方式 ：
- *          一张 为期一天 的通行证售价为 costs[0] 美元；
- *          一张 为期七天 的通行证售价为 costs[1] 美元；
- *          一张 为期三十天 的通行证售价为 costs[2] 美元。
- * 通行证允许数天无限制的旅行。 例如，如果我们在第 2 天获得一张 为期 7 天 的通行证，那么我们可以连着旅行 7 天：第 2 天、第 3 天、第 4 天、第 5 天、第 6 天、第 7 天和第 8 天。
- * 返回 你想要完成在给定的列表 days 中列出的每一天的旅行所需要的最低消费 。
- *
  * https://leetcode.cn/problems/minimum-cost-for-tickets
  */
 public class MinCostTickets {
@@ -28,23 +20,23 @@ public class MinCostTickets {
     public static int[] duration = {1, 7, 30};
 
     /**
-     * 暴力尝试
+     * 暴力递归
      *
-     * @param days
-     * @param costs
+     * @param days  日期数组
+     * @param costs 花费数组
      * @return
      */
-    public int minCostTickets1(int[] days, int[] costs) {
+    public static int minCostTickets1(int[] days, int[] costs) {
         return f1(days, costs, 0);
     }
 
     /**
-     * days[i..... 最小花费是多少
+     * 暴力递归的尝试策略函数
      *
-     * @param days
-     * @param cost
-     * @param i
-     * @return
+     * @param days 日期数组
+     * @param cost 花费数组
+     * @param i    处理第 i 天的位置
+     * @return     返回最小的花费
      */
     public static int f1(int[] days, int[] cost, int i) {
         if (i == days.length) {
@@ -63,10 +55,49 @@ public class MinCostTickets {
     }
 
     /**
-     * 使用动态规划
+     * 暴力递归 + 缓存表 = 记忆化搜素哦
      *
-     * @param days
-     * @param costs
+     * @param days  日期数组
+     * @param costs 花费数组
+     * @return
+     */
+    public static int minCostTickets2(int[] days, int[] costs) {
+        int[] dp = new int[days.length + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        return f2(days, costs, 0, dp);
+    }
+
+    /**
+     * 暴力递归 + 缓存表 = 记忆化搜索
+     *
+     * @param days 日期数组
+     * @param cost 花费数组
+     * @param i    处理第 i 天的位置
+     * @return     返回最小的花费
+     */
+    public static int f2(int[] days, int[] cost, int i, int[] dp) {
+        if(i == days.length) {
+            return 0;
+        }
+        if(dp[i] != Integer.MAX_VALUE) {
+            return dp[i];
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int k = 0, j = i; k < 3; k++) {
+            while(j < days.length && days[i] + duration[k] > days[j]) {
+                j++;
+            }
+            ans = Math.min(ans, cost[k] + f2(days, cost, j, dp));
+        }
+        dp[i] = ans;
+        return ans;
+    }
+
+    /**
+     * 动态规划
+     *
+     * @param days  日期数组
+     * @param costs 花费数组
      * @return
      */
     public static int minCostTickets(int[] days, int[] costs) {

2619d73de66aaf87d0bfa1c600a4f1fa0dbdf1cb
Sunnnnny <2441664255@qq.com>
2025-01-05 22:38:37 +0800

    【数学】：（1）使用数学公式直接配合曼哈顿距离进行计算，进行判断（2）内容补充

--- src/main/java/practice_questions/math/medium/IsReachableAtTime.java
+++ src/main/java/practice_questions/math/medium/IsReachableAtTime.java
@@ -12,17 +12,18 @@ package practice_questions.math.medium;
 public class IsReachableAtTime {
 
     /**
-     * 根据数据量猜解法，10 ^ 9 说明一定是数学公式解决的问题
+     * 根据数据量猜解法，10 ^ 9 说明一定是数学公式解决的问题，因为会出现 TLE 的限制是 10 ^ 8，所以一定使用公式方法进行求解
+     * 求从一个点到一个点可以距离：曼哈顿记录
      *
-     * @param sx
-     * @param sy
-     * @param fx
-     * @param fy
-     * @param t
+     * @param sx 起始点 x
+     * @param sy 起始点 y
+     * @param fx 终止点 x
+     * @param fy 终止点 y
+     * @param t  允许的步数
      * @return
      */
     public static boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        if(sx == fx && sy == fy) {
+        if (sx == fx && sy == fy) {
             return t != 1;
         }
         return Math.max(Math.abs(sx - fx), Math.abs(sy - fy)) <= t;

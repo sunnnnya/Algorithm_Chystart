1ea3003c5fd51bbe92fa4988c4c23b381754de58
Sunnnnny <2441664255@qq.com>
2025-01-10 00:55:14 +0800

    【暴力递归 -> 记忆化搜索 -> 动态规划】：（1）暴力递归 + 尝试策略

---
+++ src/main/java/practice_questions/dynamic_program/medium/MaxMoves.java
@@ -0,0 +1,84 @@
+package practice_questions.dynamic_program.medium;
+
+/**
+ * @BelongsPackage: practice_questions.dynamic_program.medium
+ * @ClassName: MaxMoves
+ * @Author: 丛虹羽
+ * @Date: 2025/1/10 上午12:50
+ * @Description: 矩阵中移动的最大次数
+ *
+ * leetcode: https://leetcode.cn/problems/maximum-number-of-moves-in-a-grid/description/
+ */
+public class MaxMoves {
+
+    /**
+     * 暴力递归 + 尝试策略
+     *  TLE
+     *
+     * @param grid
+     * @return
+     */
+    public static int maxMoves1(int[][] grid) {
+        int m = grid.length, ans = 0;
+        for(int i = 0; i < m; i++) {
+            ans = Math.max(ans, dfs1(grid, i, 0));
+        }
+        return ans;
+    }
+
+    /**
+     * 函数含义：从(x, y)向三个方向走能达到的最大深度
+     *
+     * @param grid 原始数组
+     * @param x    坐标 x
+     * @param y    坐标 y
+     * @return
+     */
+    public static int dfs1(int[][] grid, int x, int y) {
+        if(x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        if(get(grid, x - 1, y + 1) > grid[x][y]) {
+            max = Math.max(max, dfs1(grid, x - 1, y + 1));
+        }
+        if(get(grid, x, y + 1) > grid[x][y]) {
+            max = Math.max(max, dfs1(grid, x, y + 1));
+        }
+        if(get(grid, x + 1, y + 1) > grid[x][y]) {
+            max = Math.max(max, dfs1(grid, x + 1, y + 1));
+        }
+        return max == Integer.MIN_VALUE ? 0 : max + 1;
+    }
+
+    /**
+     * 处理越界
+     *
+     * @param grid 原始数组
+     * @param x    坐标 x
+     * @param y    坐标 y
+     * @return     越界返回 0，反之返回 grid[x][y]
+     */
+    public static int get(int[][] grid, int x, int y) {
+        if(x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
+            return 0;
+        }
+        return grid[x][y];
+    }
+
+    /**
+     * 测试
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[][] grid = {
+                {2, 4, 3, 5},
+                {5, 4, 9, 3},
+                {3, 4, 2, 11},
+                {10, 9, 13, 15}
+        };
+        System.out.println(maxMoves1(grid));
+        // 3
+    }
+}

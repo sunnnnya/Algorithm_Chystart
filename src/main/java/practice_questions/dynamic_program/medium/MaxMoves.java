f4efe6e88272ec1d718315b759abdc9a553a10be
Sunnnnny <2441664255@qq.com>
2025-01-10 01:02:16 +0800

    【暴力递归 -> 记忆化搜索 -> 动态规划】：（1）暴力递归 + 缓存表 = 记忆化搜索版本

--- src/main/java/practice_questions/dynamic_program/medium/MaxMoves.java
+++ src/main/java/practice_questions/dynamic_program/medium/MaxMoves.java
@@ -1,5 +1,8 @@
 package practice_questions.dynamic_program.medium;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * @BelongsPackage: practice_questions.dynamic_program.medium
  * @ClassName: MaxMoves
@@ -67,11 +70,59 @@ public class MaxMoves {
     }
 
     /**
+     * 暴力递归 + 缓存表 = 记忆化搜索
+     *
+     * @param grid 原始数组
+     * @return
+     */
+    public static int maxMoves(int[][] grid) {
+        int m = grid.length, n = grid[0].length, ans = 0;
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        for(int i = 0; i < m; i++) {
+            ans = Math.max(ans, dfs(grid, i, 0, dp));
+        }
+        return ans;
+    }
+
+    /**
+     * 函数含义：从(x, y)向三个方向走能达到的最大深度
+     *
+     * @param grid 原始数组
+     * @param x    坐标 x
+     * @param y    坐标 y
+     * @return
+     */
+    public static int dfs(int[][] grid, int x, int y, int[][] dp) {
+        if(x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
+            return 0;
+        }
+        if(dp[x][y] != -1) {
+            return dp[x][y];
+        }
+        int max = Integer.MIN_VALUE;
+        if(get(grid, x - 1, y + 1) > grid[x][y]) {
+            max = Math.max(max, dfs(grid, x - 1, y + 1, dp));
+        }
+        if(get(grid, x, y + 1) > grid[x][y]) {
+            max = Math.max(max, dfs(grid, x, y + 1, dp));
+        }
+        if(get(grid, x + 1, y + 1) > grid[x][y]) {
+            max = Math.max(max, dfs(grid, x + 1, y + 1, dp));
+        }
+        dp[x][y] = max == Integer.MIN_VALUE ? 0 : max + 1;;
+        return dp[x][y];
+    }
+
+    /**
      * 测试
      *
      * @param args
      */
     public static void main(String[] args) {
+        // 暴力递归 —— TLE
         int[][] grid = {
                 {2, 4, 3, 5},
                 {5, 4, 9, 3},
@@ -79,6 +130,16 @@ public class MaxMoves {
                 {10, 9, 13, 15}
         };
         System.out.println(maxMoves1(grid));
+        // 3
+
+        // 记忆化搜索
+        int[][] grid1 = {
+                {2, 4, 3, 5},
+                {5, 4, 9, 3},
+                {3, 4, 2, 11},
+                {10, 9, 13, 15}
+        };
+        System.out.println(maxMoves(grid1));
         // 3
     }
 }

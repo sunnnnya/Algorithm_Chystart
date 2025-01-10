f7f8f9ad2ff653de912bf1ac3ebb284566caa419
Sunnnnny <2441664255@qq.com>
2025-01-10 23:30:50 +0800

    【暴力递归 -> 记忆化搜索 -> 动态规划】：（1）记忆化搜索版本，直接挂上缓存表 dp 就可以，当值不为 -1 的时候，直接返回，反之进行记录。

--- src/main/java/practice_questions/dynamic_program/medium/NumDecodingsII.java
+++ src/main/java/practice_questions/dynamic_program/medium/NumDecodingsII.java
@@ -1,5 +1,7 @@
 package practice_questions.dynamic_program.medium;
 
+import java.util.Arrays;
+
 /**
  * @BelongsPackage: practice_questions.dynamic_program.medium
  * @ClassName: NumDecodingsII
@@ -20,9 +22,9 @@ public class NumDecodingsII {
      * @param s 原始字符串
      * @return  返回有效的转换方法数
      */
-    public static int numDecodings(String s) {
+    public static int numDecodings1(String s) {
         // 计算结果的过程中可能出现，爆 int 的情况，索引直接使用 long 进行处理，最后转换就可以！！！
-        return (int)dfs(s.toCharArray(), 0);
+        return (int)dfs1(s.toCharArray(), 0);
     }
 
     /**
@@ -33,14 +35,14 @@ public class NumDecodingsII {
      * @param i 索引位置
      * @return
      */
-    public static long dfs(char[] c, int i) {
+    public static long dfs1(char[] c, int i) {
         if(i == c.length) {
             return 1;
         }
         if(c[i] == '0') {
             return 0;
         }
-        long ways = dfs(c, i + 1) * (c[i] == '*' ? 9 : 1) % MOD;
+        long ways = dfs1(c, i + 1) * (c[i] == '*' ? 9 : 1) % MOD;
         // i 位置单独转：* -> 9种、1 ~ 9 -> 1种
         if(i + 1 < c.length) {
             if(c[i] != '*') {
@@ -48,15 +50,15 @@ public class NumDecodingsII {
                     // num  num
                     //  i  i + 1
                     if((c[i] - '0') * 10 + (c[i + 1] - '0') <= 26) {
-                        ways = (ways + dfs(c, i + 2) % MOD) % MOD;
+                        ways = (ways + dfs1(c, i + 2) % MOD) % MOD;
                     }
                 } else {
                     // num   *
                     //  i  i + 1
                     if(c[i] == '1') {
-                        ways = (ways + 9 * dfs(c, i + 2) % MOD) % MOD;
+                        ways = (ways + 9 * dfs1(c, i + 2) % MOD) % MOD;
                     } else if(c[i] == '2') {
-                        ways = (ways + 6 * dfs(c, i + 2) % MOD) % MOD;
+                        ways = (ways + 6 * dfs1(c, i + 2) % MOD) % MOD;
                     }
                 }
             } else {
@@ -64,17 +66,87 @@ public class NumDecodingsII {
                     // *   num
                     // i  i + 1
                     if(c[i + 1] <= '6') {
-                        ways = (ways + 2 * dfs(c, i + 2) % MOD) % MOD;
+                        ways = (ways + 2 * dfs1(c, i + 2) % MOD) % MOD;
                     } else {
-                        ways = (ways + dfs(c, i + 2) % MOD) % MOD;
+                        ways = (ways + dfs1(c, i + 2) % MOD) % MOD;
                     }
                 } else {
                     // *    *
                     // i  i + 1
-                    ways = (ways + 15 * dfs(c, i + 2) % MOD) % MOD;
+                    ways = (ways + 15 * dfs1(c, i + 2) % MOD) % MOD;
                 }
             }
         }
+        return ways;
+    }
+
+    /**
+     * 暴力递归 + 缓存表 = 记忆化搜索
+     *
+     * @param s 原始字符串
+     * @return  返回有效的转换方法数
+     */
+    public static int numDecodings(String s) {
+        char[] c = s.toCharArray();
+        long[] dp = new long[c.length];
+        Arrays.fill(dp, -1);
+        return (int)dfs(c, 0, dp);
+    }
+
+    /**
+     * 暴力递归函数的定义：
+     *      从 i 到 结束 位置返回的方法数
+     *
+     * @param c 字符数组
+     * @param i 索引位置
+     * @return
+     */
+    public static long dfs(char[] c, int i, long[] dp) {
+        if(i == c.length) {
+            return 1;
+        }
+        if(c[i] == '0') {
+            return 0;
+        }
+        if(dp[i] != -1) {
+            return dp[i];
+        }
+        long ways = dfs(c, i + 1, dp) * (c[i] == '*' ? 9 : 1) % MOD;
+        // i 位置单独转：* -> 9种、1 ~ 9 -> 1种
+        if(i + 1 < c.length) {
+            if(c[i] != '*') {
+                if(c[i + 1] != '*') {
+                    // num  num
+                    //  i  i + 1
+                    if((c[i] - '0') * 10 + (c[i + 1] - '0') <= 26) {
+                        ways = (ways + dfs(c, i + 2, dp) % MOD) % MOD;
+                    }
+                } else {
+                    // num   *
+                    //  i  i + 1
+                    if(c[i] == '1') {
+                        ways = (ways + 9 * dfs(c, i + 2, dp) % MOD) % MOD;
+                    } else if(c[i] == '2') {
+                        ways = (ways + 6 * dfs(c, i + 2, dp) % MOD) % MOD;
+                    }
+                }
+            } else {
+                if(c[i + 1] != '*') {
+                    // *   num
+                    // i  i + 1
+                    if(c[i + 1] <= '6') {
+                        ways = (ways + 2 * dfs(c, i + 2, dp) % MOD) % MOD;
+                    } else {
+                        ways = (ways + dfs(c, i + 2, dp) % MOD) % MOD;
+                    }
+                } else {
+                    // *    *
+                    // i  i + 1
+                    ways = (ways + 15 * dfs(c, i + 2, dp) % MOD) % MOD;
+                }
+            }
+        }
+        dp[i] = ways;
         return ways;
     }
 
@@ -84,13 +156,13 @@ public class NumDecodingsII {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println(numDecodings("*"));
+        System.out.println(numDecodings1("*"));
         // 9
 
-        System.out.println(numDecodings("1*"));
+        System.out.println(numDecodings1("1*"));
         // 18
 
-        System.out.println(numDecodings("2*"));
+        System.out.println(numDecodings1("2*"));
         // 15
     }
 }

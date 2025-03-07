1fb993bff583481fd464bee8c9a4cf8bd3ae1b34
Sunnnnny <2441664255@qq.com>
2025-03-07 23:46:48 +0800

    内容修改；

--- src/main/java/leetcode/skill/list/ListNode.java
+++ src/main/java/leetcode/skill/list/ListNode.java
@@ -8,19 +8,12 @@ package leetcode.skill.list;
  * @Description: 单链表中的节点信息
  */
 public class ListNode {
-
     public int val;
     public ListNode next;
 
-    public ListNode() {
-
-    }
-
+    public ListNode() {}
     public ListNode(int val) {
         this.val = val;
     }
-
-    public ListNode(int val, ListNode next) {
-        this.val = val; this.next = next;
-    }
+    public ListNode(int val, ListNode next) {this.val = val; this.next = next;}
 }

167374018178e160d3763a39fcf1f2bfb0443b28
conghongyu <2441664255@qq.com>
2025-03-10 17:32:06 +0800

    【skill】：使用数组的方式是实现栈。

--- src/main/java/leetcode/skill/stack/ImplementStackWithArray.java
+++ src/main/java/leetcode/skill/stack/ImplementStackWithArray.java
@@ -1,13 +1,13 @@
-package basic_knowledge.stack;
+package leetcode.skill.stack;
 
 /**
  * @BelongsPackage: basic_knowledge.stack
  * @ClassName: ImplementStackUseArray
  * @Author: 丛虹羽
  * @Date: 2024/7/22 下午11:59
- * @Description: 使用数组的方式实现栈
+ * @Description: 使用数组的方式实现栈 —— 常数时间比 stack 好
  */
-public class ImplementStackUseArray {
+public class ImplementStackWithArray {
 
     /**
      * 自定义stack结构
@@ -26,7 +26,7 @@ public class ImplementStackUseArray {
          * 入栈，size 表示要填入的位置
          * stack[size++] = val; -> stack[0] = val | size = 1
          *
-         * @param val
+         * @param val 待添加的元素
          */
         public void push(int val) {
             stack[size++] = val;
@@ -35,7 +35,7 @@ public class ImplementStackUseArray {
         /**
          * 查看栈顶元素（不删除）
          *
-         * @return
+         * @return r 是下一个元素要添加的位置，所以需要 return stack[r - 1]
          */
         public int peek() {
             return stack[size - 1];
@@ -44,7 +44,7 @@ public class ImplementStackUseArray {
         /**
          * 返回栈顶元素（删除）
          *
-         * @return
+         * @return 返回栈顶的元素同时删除，使用前置自减进行处理
          */
         public int pop() {
             return stack[--size];
@@ -56,7 +56,7 @@ public class ImplementStackUseArray {
          * @return
          */
         public int size() {
-            // size 表示的是元素待存储的位置
+            // size 表示的是元素待存放的位置，也表示栈中元素的个数
             return size;
         }
 
@@ -77,15 +77,15 @@ public class ImplementStackUseArray {
         stack.push(10);
         stack.push(9);
         stack.push(4);
-        System.out.println(stack.peek()); // 4
-        System.out.println(stack.size()); // 5
-        System.out.println(stack.pop()); // 4
+        System.out.println(stack.peek());  // 4
+        System.out.println(stack.size());  // 5
+        System.out.println(stack.pop());   // 4
         System.out.println(stack.empty()); // false
-        System.out.println(stack.pop()); // 9
-        System.out.println(stack.pop()); // 10
-        System.out.println(stack.pop()); // 32
-        System.out.println(stack.pop()); // 12
-        System.out.println(stack.size); // 0
+        System.out.println(stack.pop());   // 9
+        System.out.println(stack.pop());   // 10
+        System.out.println(stack.pop());   // 32
+        System.out.println(stack.pop());   // 12
+        System.out.println(stack.size);    // 0
         System.out.println(stack.empty()); // true
     }
 }

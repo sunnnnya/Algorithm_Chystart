81c1abab38ac119c59ddf9544b60d87c57a1eb30
Sunnnnny <2441664255@qq.com>
2025-03-25 12:23:58 +0800

    【二分搜索】：如果想使用二搜搜索进行快速查找时，需要找好可以二分的策略。

---
+++ src/main/java/job_interview/array/search/binary_search/BinarySearch.java
@@ -0,0 +1,110 @@
+package job_interview.array.search.binary_search;
+
+/**
+ * @BelongsPackage: job_interview.array.search.binary_search
+ * @ClassName: BinarySearch
+ * @Author: 丛虹羽
+ * @Date: 2025/3/25 11:59
+ * @Description: 二分搜索
+ */
+public class BinarySearch {
+
+    /**
+     * 二分搜索基础算法 —— 查找某个数是否存在
+     *
+     * @param arr    原始数组
+     * @param target 目标值
+     * @return
+     */
+    public static int binarySearchEqualVal(int[] arr, int target) {
+        if(arr == null || arr.length == 0) {
+            return -1;
+        }
+        int l = 0, r = arr.length - 1;
+        // 一定要写相等，否则 l == r 的时候该值是没有判断的
+        while(l <= r) {
+            int mid = r + ((l - r) >> 1);
+            if(arr[mid] < target) {
+                l = mid + 1;
+            } else if(arr[mid] > target) {
+                r = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 二分搜索 —— 找到 >= target 最左的位置
+     *
+     * @param arr    原始数组
+     * @param target 目标值
+     * @return
+     */
+    public static int binarySearchMoreEqualVal(int[] arr, int target) {
+        if(arr == null || arr.length == 0) {
+            return -1;
+        }
+        int l = 0, r = arr.length - 1;
+        while(l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if(arr[mid] >= target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+    /**
+     * 二分搜索 —— 找到 <= target 最右的位置
+     *
+     * @param arr    原始数组
+     * @param target 目标值
+     * @return
+     */
+    public static int binarySearchLessEqualVal(int[] arr, int target) {
+        if(arr == null || arr.length == 0) {
+            return -1;
+        }
+        int l = 0, r = arr.length - 1;
+        while(l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if(arr[mid] <= target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return r;
+    }
+
+    /**
+     * 测试
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 5, 7, 10, 11, 13, 19, 20};
+        System.out.println("index: " + binarySearchEqualVal(arr, 11));
+        // index: 4
+
+        int[] arr1 = new int[]{2, 5, 7, 10, 11, 13, 19, 20};
+        System.out.println("index: " + binarySearchEqualVal(arr1, 14));
+        // index: -1
+
+        int[] arr2 = new int[]{2, 5, 7, 10, 10, 10, 19, 20};
+        System.out.println("index: " + binarySearchMoreEqualVal(arr2, 10));
+        // index: 3
+
+        int[] arr3 = new int[]{2, 5, 7, 10, 10, 10, 19, 20};
+        System.out.println("index: " + binarySearchMoreEqualVal(arr3, 19));
+        // index: 6
+
+        int[] arr4 = new int[]{2, 5, 7, 10, 10, 10, 19, 20};
+        System.out.println("index: " + binarySearchLessEqualVal(arr4, 10));
+        // index: 5
+    }
+}
---
+++ src/main/java/job_interview/array/search/binary_search/FindPeakElement.java
@@ -0,0 +1,56 @@
+package job_interview.array.search.binary_search;
+
+/**
+ * @package: leetcode.skill.array.binary_search
+ * @author: chystart
+ * @create: 2025-03-10 13:43
+ * @description: 寻找峰值
+ *
+ * leetcode: https://leetcode.cn/problems/find-peak-element/description/
+ **/
+public class FindPeakElement {
+
+    /**
+     * 无序数组的 二分搜索
+     *     重点：要找到可以二分的策略，一次砍一半！！
+     *
+     * @param nums 原始数组
+     * @return
+     */
+    public static int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if(n == 1 || nums[0] > nums[1]) {
+            return 0;
+        }
+        if(nums[n - 1] > nums[n - 2]) {
+            return n - 1;
+        }
+        int l = 1, r = n - 2;
+        while(l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if(nums[mid - 1] > nums[mid]) {
+                r = mid - 1;
+            } else if (nums[mid] < nums[mid + 1]) {
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 测试
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 1, 3, 5, 6, 4};
+        System.out.println("找到最高的山峰的索引：" + findPeakElement(arr));
+        // 找到最高的山峰的索引：5
+
+        int[] arr1 = new int[]{1, 2, 3, 1};
+        System.out.println("找到最高的山峰的索引：" + findPeakElement(arr1));
+        // 找到最高的山峰的索引：2
+    }
+}

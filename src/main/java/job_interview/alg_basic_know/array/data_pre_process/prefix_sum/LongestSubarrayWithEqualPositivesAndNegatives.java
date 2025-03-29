ad03965d095a06b2b9a3b6a14d0c61f46f54ecc1
Sunnnnny <2441664255@qq.com>
2025-03-29 11:31:36 +0800

    【前缀和】：（1）对原始数组进行转换，变成 -1 0 1 的数组，使用使用前缀和的技巧搭配最早出现的位置进行求解。

---
+++ src/main/java/job_interview/alg_basic_know/array/data_pre_process/prefix_sum/LongestSubarrayWithEqualPositivesAndNegatives.java
@@ -0,0 +1,51 @@
+package job_interview.alg_basic_know.array.data_pre_process.prefix_sum;
+
+import java.util.HashMap;
+
+/**
+ * @BelongsPackage: job_interview.alg_basic_know.array.data_pre_process.prefix_sum
+ * @ClassName: LongestSubarrayWithEqualPositivesAndNegatives
+ * @Author: 丛虹羽
+ * @Date: 2025/3/29 11:19
+ * @Description: 判断数组中出现整数和负数相同的个数
+ * https://www.nowcoder.com/practice/545544c060804eceaed0bb84fcd992fb
+ */
+public class LongestSubarrayWithEqualPositivesAndNegatives {
+
+    /**
+     * 前缀和的基本使用
+     *
+     * @param nums 原始数组
+     * @return 最长子数组长度
+     */
+    public static int longestSubarrayWithEqualPositivesAndNegatives(int[] nums) {
+        int mx = Integer.MIN_VALUE;
+        int[] arr = new int[nums.length];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for(int i = 0; i < nums.length; i++) {
+            arr[i] = nums[i] > 0 ? 1 : (nums[i] == 0 ? 0 : -1);
+        }
+        for(int i = 0, sum = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if(map.containsKey(sum)) {
+                mx = Math.max(mx, i - map.get(sum));
+            }
+            if(!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        return mx;
+    }
+
+    /**
+     * 测试
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, -2, 0, 1, 2, -2, -4, -9};
+        System.out.println(longestSubarrayWithEqualPositivesAndNegatives(arr));
+        // 7
+    }
+}

e4726bdee656ae706c0776bbf9aa584d1e7a8ef9
Sunnnnny <2441664255@qq.com>
2025-04-15 00:40:55 +0800

    【链表】：使用哨兵节点免除条件判断逻辑，移动 len - n 步后，执行删除逻辑。

--- src/main/java/job_interview/alg_basic_know/list/ext/ListNode.java
+++ src/main/java/job_interview/alg_basic_know/list/ext/ListNode.java
@@ -8,16 +8,16 @@ package job_interview.alg_basic_know.list.ext;
  * @Description: 单链表的结构
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
-    ListNode() {
+    public ListNode() {
 
     }
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val; this.next = next;
     }
 }
---
+++ src/main/java/job_interview/code_top/single_list/RemoveNthFromEnd.java
@@ -0,0 +1,84 @@
+package job_interview.code_top.single_list;
+
+import job_interview.alg_basic_know.list.ext.ListNode;
+
+/**
+ * @BelongsPackage: job_interview.code_top.single_list
+ * @ClassName: RemoveNthFromEnd
+ * @Author: 丛虹羽
+ * @Date: 2025/4/15 00:30
+ * @Description: 删除链表的倒数第 N 个结点
+ * https://leetcode.cn/problems/remove-nth-node-from-end-of-list/
+ */
+public class RemoveNthFromEnd {
+
+    /**
+     * 使用哨兵节点减少条件判断
+     *
+     * @param head 链表的头节点
+     * @param n    N 序号
+     * @return
+     */
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        int len = getLength(head);
+        for(int i = 0; i < len - n; i++) {
+            cur = cur.next;
+        }
+        cur.next = cur.next.next;
+        return dummy.next;
+    }
+
+    /**
+     * 获取数组长度
+     *
+     * @param head 链表头节点
+     * @return
+     */
+    private static int getLength(ListNode head) {
+        int len = 0;
+        while(head != null) {
+            len++;
+            head = head.next;
+        }
+        return len;
+    }
+
+    /**
+     * 打印链表信息
+     *
+     * @param head 链表头节点
+     */
+    private static void print(ListNode head) {
+        while(head != null) {
+            if (head.next == null) {
+                System.out.print(head.val);
+            } else {
+                System.out.print(head.val + " -> ");
+            }
+            head = head.next;
+        }
+    }
+
+    /**
+     * 测试
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        ListNode res = removeNthFromEnd(node1, 2);
+        print(res);
+        // 1 -> 2 -> 3 -> 5
+    }
+}

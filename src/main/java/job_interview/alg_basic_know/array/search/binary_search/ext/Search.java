f0850d17d380d6ca6d4786ed8c6a1790d8f9008a
Sunnnnny <2441664255@qq.com>
2025-04-15 00:42:36 +0800

    【二分搜索】：使用二分搜索找到旋转数组的最小值，然后分开两个有序递增数组进行常规二分。

---
+++ src/main/java/job_interview/alg_basic_know/array/search/binary_search/ext/Search.java
@@ -0,0 +1,82 @@
+package job_interview.alg_basic_know.array.search.binary_search.ext;
+
+/**
+ * @BelongsPackage: job_interview.alg_basic_know.array.search.binary_search.ext
+ * @ClassName: Search
+ * @Author: 丛虹羽
+ * @Date: 2025/4/14 09:52
+ * @Description: 搜索旋转排序数组
+ * https://leetcode.cn/problems/search-in-rotated-sorted-array/description/
+ */
+public class Search {
+
+    /**
+     * 两次二分
+     *
+     * @param nums   原始数组
+     * @param target 目标值
+     * @return
+     */
+    public static int search(int[] nums, int target) {
+        int minIdx = findMin(nums);
+        if(nums[minIdx] == target) {
+            return minIdx;
+        }
+        if(target > nums[nums.length - 1]) {
+            return lowerBound(nums, 0, minIdx - 1, target);
+        }
+        return lowerBound(nums, minIdx + 1, nums.length - 1, target);
+    }
+
+    /**
+     * 返回最小值的 index
+     *
+     * @param nums 原始数组
+     * @return
+     */
+    private static int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1, target = nums[nums.length - 1];
+        while(l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if(nums[mid] > target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+
+    /**
+     * 标准二分
+     *
+     * @param nums   原始数组
+     * @param l      左边界
+     * @param r      右边界
+     * @param target 目标值
+     * @return
+     */
+    private static int lowerBound(int[] nums, int l, int r, int target) {
+        while(l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if(nums[mid] > target) {
+                r = mid - 1;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 测试
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] nums = new int[]{4,5,6,7,0,1,2};
+        System.out.println(search(nums, 0));
+    }
+}

65e20e9efe4231cf181bef8a55c45afdaae3f0bc
Sunnnnny <2441664255@qq.com>
2025-05-02 18:55:32 +0800

    feat: 实现 Runnable 接口的方式创建线程。

---
+++ src/main/java/job_interview/JUC/create_thread/MyRunnableExample.java
@@ -0,0 +1,39 @@
+package job_interview.JUC.create_thread;
+
+/**
+ * @BelongsPackage: job_interview.JUC.create_thread
+ * @ClassName: MyRunnableExample
+ * @Author: 丛虹羽
+ * @Date: 2025/5/2 18:50
+ * @Description: 继承 Runnable 接口创建线程
+ */
+public class MyRunnableExample implements Runnable {
+    @Override
+    public void run() {
+        for(int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("SubThread create thread by extending thread class!");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * 测试
+     *
+     * @param args
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // SubThread
+        Thread thread = new Thread(new MyRunnableExample());
+        thread.start();
+
+        // MainThread
+        for(int i = 1; i <= 10; i++) {
+            Thread.sleep(500);
+            System.err.println("Main thread invoke!");
+        }
+    }
+}

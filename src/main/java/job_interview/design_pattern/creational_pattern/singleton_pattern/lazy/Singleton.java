39a36956ac99b8313150510497ca4fa9905584d0
Sunnnnny <2441664255@qq.com>
2025-05-08 23:04:47 +0800

    feat: DCL + volatile 实现单例模式。

---
+++ src/main/java/job_interview/design_pattern/creational_pattern/singleton_pattern/lazy/Singleton.java
@@ -0,0 +1,27 @@
+package job_interview.design_pattern.creational_pattern.singleton_pattern.lazy;
+
+/**
+ * @BelongsPackage: job_interview.design_pattern.creational_pattern.singleton_pattern.lazy
+ * @ClassName: Singleton
+ * @Author: 丛虹羽
+ * @Date: 2025/5/8 22:57
+ * @Description: 懒汉式单例模式
+ * https://blog.csdn.net/weixin_38983929/article/details/103006376
+ */
+public class Singleton {
+
+    private static volatile Singleton instance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+           synchronized (Singleton.class) {
+               if (instance == null) {
+                   instance = new Singleton();
+               }
+           }
+        }
+        return instance;
+    }
+}

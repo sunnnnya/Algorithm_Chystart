d9bf15a1b4812226a1538cb1843ec6030f3e5bfe
Sunnnnny <2441664255@qq.com>
2025-05-14 21:40:32 +0800

    feat：三数之和问题。

--- src/main/java/job_interview/code_top/array/double_pointer/ThreeSum.java
+++ src/main/java/job_interview/code_top/array/double_pointer/ThreeSum.java
@@ -55,6 +55,6 @@ public class ThreeSum {
     public static void main(String[] args) {
         int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
         System.out.println(threeSum(nums));
-        //
+        // [[-1, -1, 2], [-1, 0, 1]]
     }
 }

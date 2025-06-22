6f4b99c5221d28d3258df906e9271653bb4b75d4
Sunnnnny <2441664255@qq.com>
2025-06-22 22:53:15 +0800

    feat：reflection 实现，测试 getDeclaredMethod() 和 getMethods() 之间的区别。

---
+++ src/main/java/job_interview/javase/reflection/method/ReflectionMethod.java
@@ -0,0 +1,65 @@
+package job_interview.javase.reflection.method;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+/**
+ * @BelongsPackage: job_interview.javase.reflection.method
+ * @ClassName: ReflectionMethod
+ * @Author: 丛虹羽
+ * @Date: 2025/6/22 22:36
+ * @Description: 反射方法
+ */
+public class ReflectionMethod {
+
+    /**
+     * 测试
+     *
+     * @param args 参数
+     */
+    public static void main(String[] args) throws Exception{
+        Class<?> studentClazz = Class.forName("job_interview.javase.reflection.method.ReflectionMethod$Student");
+
+        Constructor<?> constructor = studentClazz.getConstructor(String.class, Integer.class, Double.class);
+        Object student = constructor.newInstance("chystart", 18, 99.8);
+
+        for (Method method : studentClazz.getDeclaredMethods()) {
+            System.out.println("name: -> " + method.getName());
+        }
+
+        Method studyLanguage = studentClazz.getDeclaredMethod("studyLanguage", String.class);
+        studyLanguage.setAccessible(true);
+        Object res = studyLanguage.invoke(student, "English");
+        System.out.println("english = " + res);
+
+        // getMethods
+        for (Method method : studentClazz.getMethods()) {
+            System.out.println("name: => " + method.getName());
+        }
+    }
+
+    /**
+     * 内部类
+     */
+    public static class Student {
+        private String name;
+        private Integer age;
+        private Double score;
+
+        public Student() {}
+
+        public Student(String name, Integer age, Double score) {
+            this.name = name;
+            this.age = age;
+            this.score = score;
+        }
+
+        private String studyLanguage(String language) {
+            return this.name + " like study " + language;
+        }
+
+        public Double getScoreFromMath() {
+            return this.score;
+        }
+    }
+}

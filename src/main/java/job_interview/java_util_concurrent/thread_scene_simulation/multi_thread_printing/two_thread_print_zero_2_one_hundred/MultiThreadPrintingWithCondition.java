9ef2773764171eea67849685bcca34bd56164dfb
Sunnnnny <2441664255@qq.com>
2025-07-28 18:01:49 +0800

    fix: 两个线程交替打印，修改为使用标志位 和 condition 唤醒。

---
+++ src/main/java/job_interview/java_util_concurrent/thread_scene_simulation/multi_thread_printing/two_thread_print_zero_2_one_hundred/MultiThreadPrintingWithCondition.java
@@ -0,0 +1,65 @@
+package job_interview.java_util_concurrent.thread_scene_simulation.multi_thread_printing.two_thread_print_zero_2_one_hundred;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @BelongsPackage: job_interview.java_util_concurrent.thread_scene_simulation.multi_thread_printing
+ * @ClassName: MultiThreadPrintingWithCondition
+ * @Author: 丛虹羽
+ * @Date: 2025/7/28 16:50
+ * @Description: 两个线程交替打印 0~100 的奇偶数，使用 condition 进行交替打印
+ */
+public class MultiThreadPrintingWithCondition {
+
+    private static int count = 0;
+    private static int status = 0;
+    private static int MAX_NUM = 100;
+
+    private static ReentrantLock reentrantLock = new ReentrantLock();
+    private static Condition conditionA = reentrantLock.newCondition();
+    private static Condition conditionB = reentrantLock.newCondition();
+
+    /**
+     * 测试
+     *
+     * @param args 参数
+     */
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                while(count <= MAX_NUM) {
+                    reentrantLock.lock();
+                    while(status != 0) {
+                        conditionA.await();
+                    }
+                    System.out.println(Thread.currentThread().getName() + " print number -> " + count++);
+                    status = 1;
+                    conditionB.signal();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                reentrantLock.unlock();
+            }
+        }, "Thread-A").start();
+
+        new Thread(() -> {
+            try {
+                while(count <= MAX_NUM) {
+                    reentrantLock.lock();
+                    while(status != 1) {
+                        conditionB.await();
+                    }
+                    System.out.println(Thread.currentThread().getName() + " print number -> " + count++);
+                    status = 0;
+                    conditionA.signal();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                reentrantLock.unlock();
+            }
+        }, "Thread-B").start();
+    }
+}
--- src/main/java/job_interview/java_util_concurrent/thread_scene_simulation/multi_thread_printing/two_thread_print_zero_2_one_hundred/MultiThreadPrintingWithWaitNotify.java
+++ src/main/java/job_interview/java_util_concurrent/thread_scene_simulation/multi_thread_printing/two_thread_print_zero_2_one_hundred/MultiThreadPrintingWithWaitNotify.java
@@ -1,23 +1,19 @@
-package job_interview.java_util_concurrent.thread_scene_simulation.multi_thread_printing;
-
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
+package job_interview.java_util_concurrent.thread_scene_simulation.multi_thread_printing.two_thread_print_zero_2_one_hundred;
 
 /**
  * @BelongsPackage: job_interview.java_util_concurrent.thread_scene_simulation.multi_thread_printing
- * @ClassName: MultiThreadPrintingWithCondition
+ * @ClassName: MultiThreadPrintingWithWaitNotify
  * @Author: 丛虹羽
- * @Date: 2025/7/28 16:50
- * @Description: 使用 condition 进行交替打印
+ * @Date: 2025/7/28 16:47
+ * @Description: 两个线程交替打印 0~100 的奇偶数，使用 wait() 、 notify 进行交替打印
  */
-public class MultiThreadPrintingWithCondition {
+public class MultiThreadPrintingWithWaitNotify {
 
-    private static int count = 1;
+    private static volatile int count = 0;
 
-    private static int MAX_NUM = 16;
+    private static int MAX_NUM = 100;
 
-    private static ReentrantLock reentrantLock = new ReentrantLock();
-    private static Condition condition = reentrantLock.newCondition();
+    private static Object lock = new Object();
 
     /**
      * 测试
@@ -26,42 +22,36 @@ public class MultiThreadPrintingWithCondition {
      */
     public static void main(String[] args) {
         new Thread(() -> {
-            try {
-                reentrantLock.lock();
+            synchronized (lock) {
                 while(count <= MAX_NUM) {
-                    if((count & 1) == 1) {
+                    if((count & 1) != 1) {
                         System.out.println(Thread.currentThread().getName() + " print number -> " + count++);
-                        condition.signal();
+                        lock.notify();
                     } else {
                         try {
-                            condition.await();
+                            lock.wait();
                         } catch (InterruptedException e) {
                             throw new RuntimeException(e);
                         }
                     }
                 }
-            } finally {
-                reentrantLock.unlock();
             }
         }, "Thread-A").start();
 
         new Thread(() -> {
-            try {
-                reentrantLock.lock();
+            synchronized (lock) {
                 while(count <= MAX_NUM) {
-                    if((count & 1) != 1) {
+                    if((count & 1) == 1) {
                         System.out.println(Thread.currentThread().getName() + " print number -> " + count++);
-                        condition.signal();
+                        lock.notify();
                     } else {
                         try {
-                            condition.await();
+                            lock.wait();
                         } catch (InterruptedException e) {
                             throw new RuntimeException(e);
                         }
                     }
                 }
-            } finally {
-                reentrantLock.unlock();
             }
         }, "Thread-B").start();
     }

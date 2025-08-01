a1f1ee9319a8d9e610151afee6368d08dc8f3c81
Sunnnnny <2441664255@qq.com>
2025-08-02 00:19:00 +0800

    fix: 直接使用字符串的信息进行获取，没有必要转换成 char[]。

--- src/main/java/job_interview/hot100/dynamic_programming/multi_dimensional_dynamic_programming/MinDistance.java
+++ src/main/java/job_interview/hot100/dynamic_programming/multi_dimensional_dynamic_programming/MinDistance.java
@@ -19,16 +19,14 @@ public class MinDistance {
      * @return 最小的操作次数
      */
     public static int minDistance(String word1, String word2) {
-        char[] c1 = word1.toCharArray();
-        char[] c2 = word2.toCharArray();
-        int m = c1.length;
-        int n = c2.length;
+        int m = word1.length();
+        int n = word2.length();
         int[][] dp = new int[m + 1][n + 1];
         for(int i = 0; i <= m; i++) dp[i][0] = i;
         for(int j = 0; j <= n; j++) dp[0][j] = j;
         for(int i = 1; i <= m; i++) {
             for(int j = 1; j <= n; j++) {
-                if(c1[i - 1] == c2[j - 1]) {
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
                     dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
